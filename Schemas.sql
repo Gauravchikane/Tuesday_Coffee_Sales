f71e39a16a2fd44bd42016ce814eb30fd68625cb
Gauravchikane <gauravchikane75@gmail.com>
2025-11-12 16:38:28 +0530

    Add files via upload

---
+++ Schemas.sql
@@ -0,0 +1,35 @@
+create table city
+(
+      city_id int primary key,
+	  city_name varchar(15),
+	  population  bigint,
+	  estimated_rent float,
+	  city_rank int
+);
+
+create table customers
+(
+     customer_id int primary key,
+	 customer_name varchar(25),
+	 city_id int,
+	 constraint fk_city foreign key (city_id) references city(city_id)
+);
+
+create table products
+(
+     product_id int primary key,
+	 product_name varchar(35),
+	 price float
+);
+
+create table sales 
+(
+   sale_id int primary key,
+   sale_date date,
+   product_id int,
+   customer_id int,
+   total float,
+   rating int,
+   constraint fk_products foreign key (product_id) references products(product_id),
+   constraint fk_customers foreign key (customer_id) references customers(customer_id)
+);
